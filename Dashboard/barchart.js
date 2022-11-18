fe9e488e843a7f17c9c33cf0664b73e8dc5aa235
shannongroves <shannonsgroves@verizon.net>
2022-11-18 16:03:51 -0500

    added task 2 and 4 to dashboard

---
+++ Dashboard/barchart.js
@@ -0,0 +1,188 @@
+var groupedByPlayer = new Map();
+var playerDistances = []
+var playerSpeeds = []
+var dataset;
+
+var margin = { top: 10, right: 30, bottom: 200, left: 350 },
+    width = 600
+height = 400
+
+var svg = d3.select("#barchart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")")
+
+svg.append("text")
+    .attr("id", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width - 300)
+    .attr("y", height + 130)
+    .text("Players")
+
+svg.append("text")
+    .attr("id", "y label")
+    .attr("text-anchor", "end")
+    .attr("x", -200)
+    .attr("y", -40)
+    .attr("transform", "rotate(-90)")
+    .text("Total Distance Over Season")
+
+
+var xAxis = svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+
+var yAxis = svg.append("g")
+    .attr("class", "myYaxis")
+
+var xScale = d3.scaleBand()
+    .range([0, width])
+    .padding(0.2);
+
+var yScale = d3.scaleLinear()
+    .range([height, 0]);
+
+var barchartToolTip = d3.select("#barchartBody")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+
+
+var mouseover2 = function(d) {
+    var select = d3.select('#yScaleSelect').node();
+    var option = select.options[select.selectedIndex].value
+    barchartToolTip
+        .html("Player: " + d.group + "<br> " + option + ": " + d.value)
+        .style("opacity", 1)
+        .style("left", (d3.mouse(this)[0] + 200) + "px")
+        .style("top", (d3.mouse(this)[1] + 2050) + "px")
+        .style('position', 'absolute')
+
+}
+
+var mousemove2 = function(d) {
+    toolTip
+        .style("left", (d3.mouse(this)[0] + 200) + "px")
+        .style("top", (d3.mouse(this)[1] + 2050) + "px")
+        .style('position', 'absolute')
+}
+
+
+var mouseleave2 = function(d) {
+    barchartToolTip
+        .style("opacity", 0)
+}
+
+
+Promise.all([
+    d3.csv("tracking2018_gameId_2018123001.csv")
+    //d3.csv("file2.csv"),
+]).then(function(files) {
+    // files[0] will contain file1.csv
+    // files[1] will contain file2.csv
+    var filteredData = files[0].filter(row => { return row.displayName !== 'football' })
+    groupedByPlayer = d3.group(filteredData, d => d.displayName)
+
+    groupedByPlayer.forEach((games, player) => {
+        var totalDistance = games.reduce((accumulator, row) => {
+            row.dis = +row.dis
+            return accumulator + row.dis
+        }, 0)
+
+        var totalSpeed = games.reduce((accumulator, row) => {
+            row.s = +row.s
+            return accumulator + row.s
+        }, 0)
+
+
+        playerDistances.push({ group: player, value: +totalDistance.toFixed(2) })
+        playerSpeeds.push({ group: player, value: +(totalSpeed / games.length).toFixed(2) })
+
+    })
+
+    dataset = playerDistances
+    updateYScale()
+    updateXScale(dataset)
+
+
+}).catch(function(err) {
+    // handle error here
+})
+
+
+
+function onYScaleChanged() {
+    var select = d3.select('#yScaleSelect').node();
+    var option = select.options[select.selectedIndex].value
+
+
+    if (option === 'Total Distance') {
+        dataset = playerDistances
+        document.getElementById("y label").textContent = "Total Distance Over Season";
+    } else {
+        dataset = playerSpeeds
+        document.getElementById("y label").textContent = "Average Speed Over Season";
+    }
+
+    document.getElementById("x label").textContent = "Players"
+    updateYScale()
+    updateXScale(dataset)
+
+}
+
+function onButtonPressed() {
+    var numberPlayers = document.getElementById("numberPlayers").value;
+
+    var zoomedDataset = dataset.slice(0, numberPlayers)
+    document.getElementById("x label").textContent = "Top " + numberPlayers + " Players";
+    updateXScale(zoomedDataset)
+
+    document.getElementById("numberPlayers").value = ""
+}
+
+
+function updateYScale() {
+
+    dataset.sort(function(b, a) {
+        return a.value - b.value;
+    })
+
+    yScale.domain(d3.extent(dataset, d => { return d.value }))
+    yAxis.transition().duration(1000).call(d3.axisLeft(yScale));
+}
+
+
+function updateXScale(xDataset) {
+    xScale.domain(xDataset.map(function(d) { return d.group; }))
+    xAxis.call(d3.axisBottom(xScale))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("font", "sans-serif")
+        .style("text-anchor", "end");
+
+    var rect = svg.selectAll("rect")
+        .data(xDataset)
+
+    rect.enter()
+        .append("rect")
+        .merge(rect)
+        .on("mouseover", mouseover2)
+        .on("mouseleave", mouseleave2)
+        .on("mousemove", mousemove2)
+        .transition()
+        .duration(1000)
+        .attr("x", function(d) { return xScale(d.group); })
+        .attr("y", function(d) { return yScale(d.value); })
+        .attr("width", xScale.bandwidth())
+        .attr("height", function(d) { return height - yScale(d.value); })
+        .attr("fill", "#69b3a2")
+
+
+    rect.exit().remove()
+}
---
+++ Dashboard/piechart.js
@@ -0,0 +1,229 @@
+var svg = d3.select("#piechart")
+var width = +svg.attr("width")
+var height = +svg.attr("height")
+var radius = Math.min(width, height) / 2
+var g = svg.append("g").attr("transform", "translate(" + (width / 2 + 100) + "," + (height / 2) + ")");
+
+var groupedByGame;
+var groupedByPlayer;
+var groupedByYear;
+
+var toolTip = d3.select("#piechartBody")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+
+var mouseover = function(d) {
+    toolTip
+        .html("Event: " + d.data.event + "<br>" + "Frequency: " + d.data.frequency)
+        .style("opacity", 1)
+        .style("left", (d3.mouse(this)[0] + 400) + "px")
+        .style("top", (d3.mouse(this)[1] + 1750) + "px")
+}
+
+var mousemove = function(d) {
+    toolTip
+        .style("left", (d3.mouse(this)[0] + 400) + "px")
+        .style("top", (d3.mouse(this)[1] + 1750) + "px")
+        .style('position', 'absolute')
+}
+
+var mouseleave = function(d) {
+    toolTip
+        .style("opacity", 0)
+}
+
+
+d3.csv("tracking2018_gameId_2018123001.csv").then(function(data) {
+    var filteredData = data.filter(function(row) {
+        return row.event !== 'None';
+    })
+
+    var filteredPlayers = data.filter(function(row) {
+        return row.event !== 'None' && row.displayName !== 'football';
+    })
+
+    groupedByGame = d3.group(filteredData, d => d.gameId)
+    groupedByPlayer = d3.group(filteredPlayers, d => d.displayName)
+    groupedByYear = d3.group(filteredData, d => d.gameId.substring(0, 4))
+
+
+});
+
+
+function onEnterId() {
+    var id = document.getElementById("gameText").value.toString();
+
+    if (Array.from(groupedByGame.keys()).includes(id)) {
+        var groupedByEvent = d3.group(groupedByGame.get(id), d => d.event)
+        var eventFrequencies = []
+
+        groupedByEvent.forEach((value, key) => {
+            eventFrequencies.push({
+                'event': key.replaceAll("_", " ")
+                    .replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()),
+                'frequency': value.length
+            })
+        })
+        updatePiegraph(eventFrequencies, 'event', 'frequency')
+
+    } else alert("Game ID is not valid")
+
+    document.getElementById("gameText").value = ""
+}
+
+
+function onEnterYear() {
+    var year = document.getElementById("yearText").value.toString()
+
+    if (Array.from(groupedByYear.keys()).includes(year)) {
+        var groupedByEvent = d3.group(groupedByYear.get(year), d => d.event)
+        var eventFrequencies = []
+
+
+        groupedByEvent.forEach((value, key) => {
+            eventFrequencies.push({
+                'event': key.replaceAll("_", " ")
+                    .replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()),
+                'frequency': value.length
+            })
+        })
+        updatePiegraph(eventFrequencies, 'event', 'frequency')
+
+    } else alert("Year is not valid")
+    document.getElementById("yearText").value = ""
+}
+
+function onEnterPlayer() {
+    var player = document.getElementById("playerText").value.toString()
+
+    if (Array.from(groupedByPlayer.keys()).includes(player)) {
+        var groupedByEvent = d3.group(groupedByPlayer.get(player), d => d.event)
+        var eventFrequencies = []
+
+        groupedByEvent.forEach((value, key) => {
+            eventFrequencies.push({
+                'event': key.replaceAll("_", " ")
+                    .replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()),
+                'frequency': value.length
+            })
+        })
+        updatePiegraph(eventFrequencies, 'event', 'frequency')
+
+    } else alert("Player Name is not valid")
+    document.getElementById("playerText").value = ""
+}
+
+
+function updatePiegraph(dataset, key, value) {
+    var color = d3.scaleOrdinal(d3.schemeSpectral[11]).domain(dataset)
+
+    var pie = d3.pie()
+        .sort(function(a, b) { return d3.ascending(a[value], b[value]) })
+        .value(function(d) { return d[value] });
+
+    var path = d3.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(0);
+
+    var label = d3.arc()
+        .outerRadius(radius - 40)
+        .innerRadius(radius - 40);
+
+    var arc = g.selectAll('path')
+        .data(pie(dataset))
+
+    arc.enter().append('path')
+        .on('mouseover', mouseover)
+        .on('mouseleave', mouseleave)
+        .on('mousemove', mousemove)
+        .merge(arc)
+        .attr("class", "arc")
+        .transition().duration(500)
+        .attr('d', path)
+        .attr('fill', function(d) { return (color(d.data[key])) })
+        .attr("stroke", "gray")
+
+    arc.exit().remove()
+
+
+    var text = g.selectAll('text')
+        .data(pie(dataset))
+
+    text.enter().append('text').merge(text)
+        .transition().duration(500)
+        //consider ease function
+        .text(function(d) {
+            return d.data[key]
+        })
+        .attr("transform", function(d) {
+            return "translate(" + label.centroid(d) + ")";
+        })
+        .style("text-anchor", "middle")
+        .style("font-size", 17)
+
+    text.exit().remove()
+}
+
+
+function onCheck(checkbox) {
+    switch (checkbox.id) {
+        case 'gameId':
+            if (checkbox.checked) {
+                document.getElementById("gameText").disabled = false
+                document.getElementById("gameButton").disabled = false
+                document.getElementById("yearId").disabled = true
+                document.getElementById("playerId").disabled = true
+            } else {
+                document.getElementById("gameText").disabled = true
+                document.getElementById("gameButton").disabled = true
+                document.getElementById("yearId").disabled = false
+                document.getElementById("playerId").disabled = false
+            }
+            break;
+        case 'yearId':
+            if (checkbox.checked) {
+                document.getElementById("yearText").disabled = false
+                document.getElementById("yearButton").disabled = false
+                document.getElementById("gameId").disabled = true
+                document.getElementById("playerId").disabled = true
+            } else {
+                document.getElementById("yearText").disabled = true
+                document.getElementById("yearButton").disabled = true
+                document.getElementById("gameId").disabled = false
+                document.getElementById("playerId").disabled = false
+            }
+            break;
+        case 'playerId':
+            if (checkbox.checked) {
+                document.getElementById("playerText").disabled = false
+                document.getElementById("playerButton").disabled = false
+                document.getElementById("yearId").disabled = true
+                document.getElementById("gameId").disabled = true
+            } else {
+                document.getElementById("playerText").disabled = true
+                document.getElementById("playerButton").disabled = true
+                document.getElementById("yearId").disabled = false
+                document.getElementById("gameId").disabled = false
+            }
+            break;
+    }
+}
+
+
+
+//2018123001
+
+
+//     ['kickoff', 'kick_received', 'first_contact',
+//         'tackle', 'ball_snap', 'extra_point_attempt',
+//         'out_of_bounds', 'extra_point', 'field_goal_attempt',
+//         'field_goal_missed', 'punt', 'punt_land', 'punt_received',
+//         'fair_catch', 'touchback', 'kickoff_land', 'field_goal',
+//         'punt_downed'
+//     ])
